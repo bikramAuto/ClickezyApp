e36c68e1218b53d8524d55d9ca8e010c9247df80
Bikram <bikram.nanda2@gmail.com>
2023-07-26 15:29:03 +0530

    Test result

--- src/main/java/com/clickezy/base/data/Data.java
+++ src/main/java/com/clickezy/base/data/Data.java
@@ -3,12 +3,12 @@ package com.clickezy.base.data;
 public class Data {
 	
 	//SignUp
-	protected String NewID = "demo147";
+	protected String NewID = "demo148";
 	protected String NewMail = NewID+"@mailsac.com";
 	protected String psw = "Demo@123";
 	protected String Otp = "1234";
 	protected String Name = "demo";
-	protected String mob = "9937123516";
+	protected String mob = "9937123517";
 	
 	//Studio Data
 	protected String StudioName = "Demo Studio21";
@@ -19,8 +19,8 @@ public class Data {
 	protected String zip = "765432";
 	
 	//User SighIn
-	protected String Lmail = "demo134@mailsac.com";
-	protected String Lmob = "9937123511";
+	protected String Lmail = "demo148@mailsac.com";
+	protected String Lmob = "9937123517";
 	
 	//OnBord SignIn
 	protected String Smail = "demo128@mailsac.com";
